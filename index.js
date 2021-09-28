4da41fc17272f5535cfeab41282bbf9270f4ba1a
Roby Morgan <roby.c.morgan@gmail.com>
2021-09-28 16:56:43 +0200

    JS. ex completed

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 //Ex 7
-
-function h1TextChanger () {
+const h1TextChanger = function () {
     document.getElementById("#h1").innerHTML = "Something Else"
 }
 
 //Ex8
-function backGroundImage () {
+function backGroundImage() {
     document.getElementById("#main").style.color = "lightblue"
 }
 
 //Ex9
-function changeTheFooterAddress () {
+function changeTheFooterAddress() {
     document.getElementById("#address").innerHTML = "Fake address"
 }
 
 //Ex10
-
-//Ex11
+function addClassToLinks() {
+    let amazonLinkClass = document.querySelectorAll("a")
+    amazonLinkClass.classList.add("amazon-link")
+}
+//Ex11  
 function toggleImagesDisplay() {
-    var element = document.getElementById("img-product")
-    element.classList.toggle.style.display = "none"
+    let imgClass = document.getElementsByClassName(".p1", ".p2", ".p3", ".p4", ".p5")
+    imgClass.classList.toggle.display = "none"
   }
+
+//Ex12 Write a function to color the price of each product in a different one every time it's invoked
+const color = function () {
+    let priceColor = document.getElementsByTagName(".price")
+    priceColor.classList.display.color = "orange"
+}
