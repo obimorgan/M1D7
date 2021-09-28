1d4936b1822772b3195408fe0bb0ecfb7f204f39
Roby Morgan <roby.c.morgan@gmail.com>
2021-09-28 16:31:07 +0200

    with an added toggle button

---
+++ index.js
@@ -0,0 +1,23 @@
+//Ex 7
+
+function h1TextChanger () {
+    document.getElementById("#h1").innerHTML = "Something Else"
+}
+
+//Ex8
+function backGroundImage () {
+    document.getElementById("#main").style.color = "lightblue"
+}
+
+//Ex9
+function changeTheFooterAddress () {
+    document.getElementById("#address").innerHTML = "Fake address"
+}
+
+//Ex10
+
+//Ex11
+function toggleImagesDisplay() {
+    var element = document.getElementById("img-product")
+    element.classList.toggle.style.display = "none"
+  }
